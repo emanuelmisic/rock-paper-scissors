8937cec24cfe07e2fd6b5419b1c5bdfb2bc33aa9
emanuelmisic <emanuelmisic64@gmail.com>
2022-06-19 19:28:04 +0200

    Setup game engine

--- script.js
+++ script.js
@@ -1,53 +1,104 @@
-// const plays = ["rock", "paper", "scissors"];
-// const rand = Math.floor(Math.random() * 3);
-
-// const computerPlay = plays[rand];
-
-// let player;
-// let comp;
-
-// game();
-
-// function round(playerSelection, computerSelection) {
-//   playerSelection = prompt("Enter your input:");
-//   playerSel = playerSelection.toLowerCase();
-//   if (playerSel == "rock" && computerSelection == "paper") {
-//     console.log("Computer Wins! PAPER beats ROCK!");
-//     comp += 1;
-//   } else if (playerSel == "rock" && computerSelection == "scissors") {
-//     console.log("You Win! ROCK beats SCISSORS!");
-//     player += 1;
-//   } else if (playerSel == "paper" && computerSelection == "rock") {
-//     console.log("You Win! PAPER beats ROCK!");
-//     player += 1;
-//   } else if (playerSel == "paper" && computerSelection == "scissors") {
-//     console.log("Computer Wins! SCISSORS beats PAPER!");
-//     comp += 1;
-//   } else if (playerSel == "scissors" && computerSelection == "paper") {
-//     console.log("You Win! SCISSORS beats PAPER!");
-//     player += 1;
-//   } else if (playerSel == "scissors" && computerSelection == "rock") {
-//     console.log("Computer Wins! ROCK beats SCISSORS!");
-//     comp += 1;
-//   } else {
-//     console.log(
-//       `Draw! ${playerSel.toUpperCase()} does nothing to ${computerSelection.toUpperCase()}!`
-//     );
-//   }
-// }
-
-// function game() {
-//   comp = 0;
-//   player = 0;
-//   playerPlay = "";
-//   for (let i = 0; i < 5; i++) {
-//     round(playerPlay, computerPlay);
-//   }
-//   if (comp > player) return console.log("Computer Won The Game!");
-//   if (player > comp) return console.log("You Won The Game!");
-// }
-
 const plays = ["rock", "paper", "scissors"];
 const rand = Math.floor(Math.random() * 3);
 
 const computerPlay = plays[rand];
+
+let player;
+let comp;
+let count;
+
+// DOM Elements
+const score = document.querySelector(".score");
+const state = document.querySelector(".status");
+const message = document.querySelector(".message");
+const gameTrack = document.querySelector(".game-track");
+const rockBtn = document.querySelector("#rock");
+const paperBtn = document.querySelector("#paper");
+const scissorsBtn = document.querySelector("#scissors");
+const finalScore = document.querySelector("#final");
+
+// Initialization
+rockBtn.addEventListener("click", () => {
+  const selection = rockBtn.id;
+  round(selection, computerPlay);
+});
+paperBtn.addEventListener("click", () => {
+  const selection = paperBtn.id;
+  round(selection, computerPlay);
+});
+scissorsBtn.addEventListener("click", () => {
+  const selection = scissorsBtn.id;
+  round(selection, computerPlay);
+});
+
+// The Game
+game();
+
+// Function Section
+function round(playerSelection, computerSelection) {
+  playerSel = playerSelection.toLowerCase();
+  if (playerSel == "rock" && computerSelection == "paper") {
+    state.innerHTML = "Computer Wins!";
+    message.innerHTML = "PAPER beats ROCK!";
+    comp++;
+    count++;
+    updateScore(player, comp, count);
+  } else if (playerSel == "rock" && computerSelection == "scissors") {
+    state.innerHTML = "You win!";
+    message.innerHTML = "ROCK beats SCISSORS!";
+    player++;
+    count++;
+    updateScore(player, comp, count);
+  } else if (playerSel == "paper" && computerSelection == "rock") {
+    state.innerHTML = "You win!";
+    message.innerHTML = "PAPER beats ROCK!";
+    player++;
+    count++;
+    updateScore(player, comp, count);
+  } else if (playerSel == "paper" && computerSelection == "scissors") {
+    state.innerHTML = "Computer Wins!";
+    message.innerHTML = "SCISSORS beats PAPER!";
+    comp++;
+    count++;
+    updateScore(player, comp, count);
+  } else if (playerSel == "scissors" && computerSelection == "paper") {
+    state.innerHTML = "You win!";
+    message.innerHTML = "SCISSORS beats PAPER!";
+    player++;
+    count++;
+    updateScore(player, comp, count);
+  } else if (playerSel == "scissors" && computerSelection == "rock") {
+    state.innerHTML = "Computer Wins!";
+    message.innerHTML = "ROCK beats SCISSORS!";
+    comp++;
+    count++;
+    updateScore(player, comp, count);
+  } else {
+    state.innerHTML = "No win!";
+    message.innerHTML = `Draw! ${playerSel.toUpperCase()} does nothing to ${computerSelection.toUpperCase()}!`;
+    count++;
+    updateScore(player, comp, count);
+  }
+}
+
+function updateScore(player, computer, gameNum) {
+  score.innerHTML = `Computer: ${computer} | Player: ${player}`;
+  gameTrack.innerHTML = `Game: ${gameNum}`;
+  if (count == 5) {
+    if (computer > player) {
+      finalScore.innerHTML = "Computer Won The Game!";
+      game();
+    } else if (player > computer) {
+      finalScore.innerHTML = "You Won The Game!";
+      game();
+    } else {
+      finalScore.innerHTML = "No Winner This Time!";
+      game();
+    }
+  }
+}
+
+function game() {
+  player = count = comp = 0;
+  updateScore(player, comp, count);
+}
